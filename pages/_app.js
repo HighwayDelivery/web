675074faf4c13d728c7aa7b7327e7286add4fb22
Patrick Krawczykowski <patrick@dreadful.design>
2019-10-29 16:31:33 -0500

    use pages/api routes

--- pages/_app.js
+++ pages/_app.js
@@ -6,46 +6,44 @@ import redirect from "utils/redirect"
 import Base from "components/base"
 
 export default class App extends NextApp {
-  // static async getInitialProps(appContext) {
-  //   const {
-  //     ctx: { req, res, asPath }
-  //   } = appContext
-  //   const auth = firebase.auth()
-  //   const user = req && req.session ? req.session.decodedToken : auth.currentUser
-  //   if (user && asPath === "/") redirect(`/account/${user.uid}`, res)
-  //   if (!user && asPath !== "/") redirect("/", res)
-  //   const appProps = await NextApp.getInitialProps(appContext)
-  //   return {...appProps }
-  // }
+  static async getInitialProps(appContext) {
+    const {
+      ctx: { req, res, asPath }
+    } = appContext
+    const auth = firebase.auth()
+    const user = req && req.session ? req.session.decodedToken : auth.currentUser
+    if (user && asPath === "/") redirect(`/account/${user.uid}`, res)
+    if (!user && asPath !== "/") redirect("/", res)
+    const appProps = await NextApp.getInitialProps(appContext)
+    return { ...appProps }
+  }
 
-  // state = { user: this.props.user }
+  state = { user: this.props.user }
 
   async componentDidMount() {
-    const response = await axios.post("/api/logout")
-    console.log(response)
-    // firebase.auth().onAuthStateChanged(async user => {
-    //   if (user) {
-    //     this.setState({ user })
-    //     try {
-    //       const token = await user.getIdToken()
-    //       return await axios.post("/api/login", { token })
-    //     } catch (err) {
-    //       console.log(err)
-    //     }
-    //   } else {
-    //     this.setState({ user: null })
-    //     await axios.post("/api/logout")
-    //   }
-    // })
+    firebase.auth().onAuthStateChanged(async user => {
+      if (user) {
+        this.setState({ user })
+        try {
+          const token = await user.getIdToken()
+          return await axios.post("/api/login", { token })
+        } catch (err) {
+          console.log(err)
+        }
+      } else {
+        this.setState({ user: null })
+        console.log(await axios.post("/api/logout"))
+      }
+    })
   }
 
   render() {
     const { Component, pageProps } = this.props
-    // const { user } = this.state
+    const { user } = this.state
     return (
       <>
         <Base />
-        <Component {...pageProps} />
+        <Component user={user} {...pageProps} />
       </>
     )
   }
--- server.js
+++ server.js
@@ -41,26 +41,6 @@ app.prepare().then(() => {
     next()
   })
 
-  server.post("/api/login", (req, res) => {
-    console.log("log in")
-    if (!req.body) return res.sendStatus(400)
-    const token = req.body.token
-    firebase
-      .auth()
-      .verifyIdToken(token)
-      .then(decodedToken => {
-        req.session.decodedToken = decodedToken
-        res.json({ status: true, decodedToken })
-      })
-      .catch(error => res.json({ error }))
-  })
-
-  server.post("/api/logout", (req, res) => {
-    console.log("log out")
-    req.session.decodedToken = null
-    res.json({ status: true })
-  })
-
   server.all("*", handler)
 
   server.listen(port, err => {
